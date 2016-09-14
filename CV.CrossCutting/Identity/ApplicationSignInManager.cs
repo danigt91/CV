f20399eaa55ee4995437b7f7bc3ec339e3833996
danigt91 <danigt91@gmail.com>
2016-09-14 14:24:28 +0200

    Experimental ApplicationSignInManager

---
+++ CV.CrossCutting/Identity/ApplicationSignInManager.cs
@@ -0,0 +1,36 @@
+﻿using CV.CrossCutting.Service;
+using CV.Infraestructure.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV.CrossCutting.Identity
+{
+    // Configure the application sign-in manager which is used in this application.
+    [Obsolete("No se esta utilizando esta clase para el SignIn pero hay que plantear si usar esta referencia")]
+    public class ApplicationSignInManager : SignInManager<IdentityUser, string>
+    {
+        public ApplicationSignInManager(UserManager<IdentityUser> userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((UserManager<IdentityUser>)UserManager, DefaultAuthenticationTypes.ApplicationCookie);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(OwinService.GetUserManager(context), context.Authentication);
+        }
+    }
+}
